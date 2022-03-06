02dc57aaca3c1d3b8af2ba6e8a4098318d8374ed
Andre Borges Ribeiro <andre88br@gmail.com>
2022-03-06 16:58:32 -0300

    Aula 7 - televisão

--- Introdução à programação com Python/aula7_televisao.py
+++ Introdução à programação com Python/aula7_televisao.py
@@ -6,5 +6,33 @@ class Televisao:
         self.canal = 3
 
     def botao_power(self):
-        if self.ligada == False:
+        if self.ligada:
+            self.ligada = False
+        else:
             self.ligada = True
+
+    def sobe_canal(self):
+        if self.ligada:
+            self.canal += 1
+        else:
+            print('A TV está desligada')
+
+    def desce_canal(self):
+        if self.ligada:
+            self.canal -= 1
+        else:
+            print('A TV está desligada')
+
+    def mostra_canal(self):
+        if self.ligada:
+            return self.canal
+        else:
+            return 'A televisão está desligada'
+
+
+televisao = Televisao()
+
+televisao.botao_power()
+
+print('A televisão está ligada? {}'.format(televisao.ligada))
+print('Canal: {}'.format(televisao.mostra_canal()))
