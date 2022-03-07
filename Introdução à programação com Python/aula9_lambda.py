3a52f61cf51eabd2e1282fc6016e6460c2615465
Andre Borges Ribeiro <andre88br@gmail.com>
2022-03-06 22:52:26 -0300

    Aula 11 e atualização da aula 9 com exceção

--- Introdução à programação com Python/aula9_lambda.py
+++ Introdução à programação com Python/aula9_lambda.py
@@ -1,3 +1,11 @@
+class Error(Exception):
+    pass
+
+
+class InputError(Error):
+    def __init__(self, message):
+        self.message = message
+
 
 calculadora = {
     'soma': lambda a, b: a + b,
@@ -6,28 +14,21 @@ calculadora = {
     'multiplicacao': lambda a, b: a * b
 }
 
-num1, num2 = 0, 0
-operacao = 0
-
-while num1 == 0 or type(num1) is not int:
+while True:
     try:
         num1 = int(input('N1: '))
-    except TypeError:
-        print('N1 deve ser um número inteiro!')
+        break
     except ValueError:
         print('N1 deve ser um número inteiro!')
 
-
-while num2 == 0 or type(num2) is not int:
+while True:
     try:
-        num2 = int(input('N2: '))
-    except TypeError:
-        print('N2 deve ser um número inteiro!')
+        num2 = float(input('N2: '))
+        break
     except ValueError:
-        print('N2 deve ser um número inteiro!')
+        float('N2 deve ser um número inteiro!')
 
-
-while operacao < 1 or operacao > 4 or type(operacao) is not int:
+while True:
     try:
         operacao = int(input('1 - Soma\n'
                              '2 - Subtração\n'
@@ -35,16 +36,21 @@ while operacao < 1 or operacao > 4 or type(operacao) is not int:
                              '4 - Multiplicação\n'
                              'Escolha uma das opções: '))
         if operacao == 1:
-            print('{} + {} = {}'.format(num1, num2, calculadora['soma'](num1, num2)))
+            print('{} + {} = {:.2f}'.format(num1, num2, calculadora['soma'](num1, num2)))
         elif operacao == 2:
-            print('{} + {} = {}'.format(num1, num2, calculadora['subtracao'](num1, num2)))
+            print('{} + {} = {:.2f}'.format(num1, num2, calculadora['subtracao'](num1, num2)))
         elif operacao == 3:
-            print('{} / {} = {}'.format(num1, num2, calculadora['divisao'](num1, num2)))
+            print('{} / {} = {:.2f}'.format(num1, num2, calculadora['divisao'](num1, num2)))
         elif operacao == 4:
-            print('{} * {} = {}'.format(num1, num2, calculadora['multiplicacao'](num1, num2)))
-        else:
-            print('Opção inválida! Escolha outra opção!')
+            print('{} * {} = {:.2f}'.format(num1, num2, calculadora['multiplicacao'](num1, num2)))
+        elif operacao < 1 or operacao > 4:
+            raise InputError('Opção inválida! Escolha outra opção!')
+        break
     except TypeError:
         print('Opção inválida! Escolha outra opção!')
     except ValueError:
         print('Opção inválida! Escolha outra opção!')
+    except InputError as ex:
+        print(ex)
+    except Exception as ex:
+        print('Erro desconhecido! Erro: {}'.format(ex))
